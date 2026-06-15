cee4e6eecc815cd0b1004a52b74a21717d0fe1e2
saivenkatpractice <saivenkat.practice@gmail.com>
2026-06-15 21:42:39 +0530

    feat: immutable ECR tags and deploy by image SHA (stage 7)

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -55,7 +55,7 @@ resource "aws_ecs_task_definition" "app" {
   container_definitions = jsonencode([
     {
       name                   = var.project_name
-      image                  = "${aws_ecr_repository.app.repository_url}:latest"
+      image                  = "${aws_ecr_repository.app.repository_url}:${var.image_tag}"
       essential              = true
       readonlyRootFilesystem = true
 
